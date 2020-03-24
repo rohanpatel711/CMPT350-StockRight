d6def0b1add6ad0785975afcc5cdd4854b89740e
unknown <rohanspatel711711@gmail.com>
2020-03-24 11:32:54 -0600

    Implemented the search functionality to work with the API.

--- StockRight/stockQuotes/views.py
+++ StockRight/stockQuotes/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 
+# IEX API key: pk_6e6c4c5ea9f843ca94619cf3957d691a
+
 # Homepage view function
 # Request = browser request
 # Return and render a new page (in this case the home page)
 def home(request):
-    return render(request, 'home.html', {})
+    # Used for API
+    import requests
+    import json
+
+    if request.method == "POST":
+        ticker = request.POST['ticker']
+        url = "https://cloud.iexapis.com/stable/stock/"+ticker+"/quote?token=pk_6e6c4c5ea9f843ca94619cf3957d691a"
+        api_req = requests.get(url)
+
+        try:
+            api = json.loads(api_req._content)
+        except Exception as e:
+            api = "Error"
+        return render(request, 'home.html', {'api':api})
+    else:
+        return render(request, 'home.html', {'ticker':'Enter Ticker above.'})
+
 
 # About Me page view function
 def about(request):
