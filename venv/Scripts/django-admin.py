d4f2aca3e0ea3dfc227d20fe1168b35e882eb546
unknown <rohanspatel711711@gmail.com>
2020-03-24 11:19:43 -0600

    Created a Virtual Python Environment and installed Django

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\akshar\desktop\y4s2\cmpt350\project\stockright\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
