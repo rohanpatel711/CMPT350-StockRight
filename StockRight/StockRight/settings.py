e817dddfae4fff64c746fd0b9f72a46372b5a319
unknown <rohanspatel711711@gmail.com>
2020-03-24 11:27:24 -0600

    Connected the internal Quotes app to the main stockApp.

--- StockRight/StockRight/settings.py
+++ StockRight/StockRight/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # First stock ticker app
+    'stockQuotes'
 ]
 
 MIDDLEWARE = [
