268cf123431fc5751de37393c707ca52ffeeab24
unknown <rohanspatel711711@gmail.com>
2020-03-25 19:43:06 -0600

    Added Top mentioned stock in Homepage

--- StockRight/StockNews/views.py
+++ StockRight/StockNews/views.py
@@ -5,7 +5,7 @@ def news(request):
     # Used for API
     import requests
     import json
-    url = "https://stocknewsapi.com/api/v1/category?section=alltickers&items=50&token=ubtrdde1nxakqqfeasdp1urmiz7xgxi4a1aqo25n"
+    url = "https://stocknewsapi.com/api/v1/category?section=general&items=50&token=ubtrdde1nxakqqfeasdp1urmiz7xgxi4a1aqo25n"
     api_req = requests.get(url)
     try:
         data = json.loads(api_req._content)
--- StockRight/stockQuotes/views.py
+++ StockRight/stockQuotes/views.py
@@ -12,7 +12,7 @@ def home(request):
 
     if request.method == "POST":
         ticker = request.POST['ticker']
-        url = "https://cloud.iexapis.com/stable/stock/"+ticker+"/quote?token=pk_6e6c4c5ea9f843ca94619cf3957d691a"
+        url = "https://cloud.iexapis.com/stable/stock/"+ticker+"/quote?token=pk_8ffc6e63445e477fa4c225e7f9a30694"
         api_req = requests.get(url)
 
         try:
@@ -36,10 +36,13 @@ def myStocks(request):
 def index(request):
     import requests
     import json
-    url = "https://stocknewsapi.com/api/v1/category?section=alltickers&items=50&token=ubtrdde1nxakqqfeasdp1urmiz7xgxi4a1aqo25n"
+    url = "https://stocknewsapi.com/api/v1/category?section=general&items=50&token=ubtrdde1nxakqqfeasdp1urmiz7xgxi4a1aqo25n"
+    url1 = "https://stocknewsapi.com/api/v1/top-mention?&date=last30days&token=fyrd2wiyzfxnbvm69itcoq2bzszuywolnfxcetqx"
     api_req = requests.get(url)
+    api_req1 = requests.get(url1)
     try:
         data = json.loads(api_req._content)
+        data1 = json.loads(api_req1._content)
     except Exception as e:
         api = "Error"
-    return render(request, 'index.html', {'api': data})
+    return render(request, 'index.html', {'api': data, 'api1':data1})
